3f39a1a324f80e89e0a53e44192bab11db03b405
unknown <bartosztomaszewski3@gmail.com>
2020-04-29 12:44:02 +0200

    Added sorting list from the most avavilable free space

--- mailbox.py
+++ mailbox.py
@@ -1,18 +1,29 @@
 from decimal import Decimal as D
 
 accounts = []
+accounts_dict = {}
+flag = False
+
 with open('list.txt', errors='ignore') as lh_accounts:
+    username = ''
+    
     for i in lh_accounts:
+        index_at = i.find('@')
+        if index_at != -1:
+            username = i[:index_at]
+
         if i.find('/') != -1 and i.find('.') != -1:
-            accounts.append(i.strip())
+            accounts_dict[username] = i.strip()
 
 used_space = 0
 all_space = 0
 tmp = ''
 tmp_used = ''
 tmp_all = ''
-for i in range(len(accounts)):
-    tmp = accounts[i].split('/')
+
+for username, space in accounts_dict.items():
+    #print(space)
+    tmp = space.split('/')
 
     tmp_used = tmp[0]
     tmp_used = tmp_used.split()
@@ -23,4 +34,14 @@ for i in range(len(accounts)):
     used_space += D(tmp_used[0])
     all_space += D(tmp_all[0])
 
-print('We have {USED} MB used space on users\' accounts.\nWe have {ALL} MB assigned to users\' accounts.\nWe have {AVA} MB free space on users\s accoutns.\n'.format(USED=used_space, ALL=all_space, AVA=all_space-used_space))
+    accounts_dict[username] = [float(tmp_used[0]), float(tmp_all[0]), float(D(tmp_all[0])-D(tmp_used[0]))]
+
+accounts_free_dict = {}
+for username, space in accounts_dict.items():
+    #print('Email {EMAIL}@mforce.pl\nfree: {FREE}\nused: {USED}\ntotal: {ALL}\n'.format(EMAIL=username, FREE=space[2], USED=space[0], ALL=space[1]))
+    accounts_free_dict[space[2]] = [username, space[0], space[1]]
+
+for i in sorted (accounts_free_dict, reverse=True):
+    print('Email {EMAIL}@mforce.pl\nfree: {FREE}\nused: {USED}\ntotal: {ALL}\n'.format(EMAIL=accounts_free_dict[i][0], FREE=i, USED=accounts_free_dict[i][1], ALL=accounts_free_dict[i][2]))
+
+print('We have {USED} MB used space on users\' accounts.\nWe have {ALL} MB assigned to users\' accounts.\nWe have {AVA} MB free space on users\' accounts.\n'.format(USED=used_space, ALL=all_space, AVA=all_space-used_space))
